6a4f67de810e91554c005f879a6f9d715924d82b
ScREy5 <pgeri595@gmail.com>
2022-09-29 00:29:56 +0200

    Expanded experimental setup

--- run_experiment.py
+++ run_experiment.py
@@ -1,10 +1,16 @@
 import subprocess
 
 log_dir = "experiment_results"
-langs = ["en","hu"]
+# langs = ["en","hu","nl"]
+langs = ["en"] #for testing
+pids = ["P36","P39"]
+pids_arg = ",".join(pids)
+# TODO : Dynamic pid reading from external file
 
 if __name__ == '__main__':
 
     for lang in langs:
-        subprocess.call(["python", "scripts/probe.py", "--pids", "P36", "--log_dir", log_dir+"_"+lang,
+        experiment = subprocess.run(["python", "scripts/probe.py", "--pids", pids_arg, "--log_dir", log_dir+"_"+lang,
                          "--pred_dir", log_dir+"_"+lang, "--lang", lang],shell=True)
+
+        if experiment.returncode != 0 : raise RuntimeError("Experiment failed - Subprocess exit code 1")
