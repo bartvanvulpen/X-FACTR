d3bf8f01f097ed1acaafa67ea541d9d48810a764
RutgerEttes <rutgerettes@gmail.com>
2022-10-04 15:27:38 +0200

    Update formatting.py

--- formatting.py
+++ formatting.py
@@ -3,19 +3,57 @@ from qwikidata.linked_data_interface import get_entity_dict_from_api
 import pickle
 import os
 
+def write_aliases(item):
+    labels = []
+    for lang in ['en', 'nl', 'hu']:
 
+        if lang in item['aliases'].keys():
+            aliases = item['aliases'][lang]
+            alias_f = open(f"own_alias/{lang}.txt", "a", encoding='utf-8')
+            alias_f.write(item['id'] + " ")
+            for alias in aliases:
+                alias_f.write(alias["value"])
+                alias_f.write("    ")
+
+            alias_f.write("\n")
+            alias_f.close()
+
+        if lang in item['labels'].keys():
+            labels.append( f"\t{item['labels'][lang]['value']}@{lang}")
+        uni_f = open("own_unicode_escape.txt", "a", encoding='utf-8')
+        uni_f.write(f"{item['id']}\t{''.join(labels)}\n")
+        uni_f.close()
+
+
+
+        
+multi_f = open(f"own_multi_rel.txt", "w", encoding='utf-8')
+multi_f.write("")
+multi_f.close()
+
+uni_f = open(f"own_unicode_escape.txt", "w", encoding='utf-8')
+uni_f.write("")
+uni_f.close()
+
+entity_ids = set()
 with open("jsondict.p", 'rb') as f:
     jsondict = pickle.load(f)
 
     for lang in jsondict.keys():
-        alias_f = open(f"own_alias/{lang}.txt", "w")
+        alias_f = open(f"own_alias/{lang}.txt", "w", encoding='utf-8')
         alias_f.write("")
         alias_f.close()
 
         for predicate_id in jsondict[lang].keys():
-
+        
             item = get_entity_dict_from_api(predicate_id)
-            obj_label = item['labels'][lang]['value']
+            pred_label = item['labels'][lang]['value']
+
+            c = 0
+
+            multi_f = open(f"own_facts_{lang}/{predicate_id}.jsonl", "w", encoding='utf-8')
+            multi_f.write("")
+            multi_f.close()
 
             for identity in jsondict[lang][predicate_id]:
 
@@ -25,25 +63,35 @@ with open("jsondict.p", 'rb') as f:
                     item = get_entity_dict_from_api(item)
 
                     sub_uri = item["id"]
-                    sub_label = item["labels"][lang]["value"]\
+                    if lang in item["labels"].keys():
+                        sub_label = item["labels"][lang]["value"]
+                    else:
+                        sub_label = item["labels"][list(item["labels"].keys())[0]]["value"]
+                    objs = [i['mainsnak']['datavalue']['value']['id'] for i in item['claims'][predicate_id]]
 
-                    F_f = open(f"own_facts_{lang}/{predicate_id}.jsonl", "a")
+                    for obj_id in objs:
+                        if obj_id not in entity_ids:
+                            entity_ids.add(obj_id)
+                            obj_dict = get_entity_dict_from_api(obj_id)
+
+                            write_aliases(obj_dict)
+
+                    multi_f = open(f"own_multi_rel.txt", "a", encoding='utf-8')
+                    multi_f.write(
+                        f"{sub_uri}\t{predicate_id}\t{' '.join(objs)}\n")
+                    multi_f.close()
+
+                    obj_label = get_entity_dict_from_api(objs[0])["labels"][lang]["value"]
+
+
+
+                    F_f = open(f"own_facts_{lang}/{predicate_id}.jsonl", "a", encoding='utf-8')
 
                     F_f.write(
-                        "{" + f'"uuid": xD, "predicate_id": "{predicate_id}", "sub_uri": "{sub_uri}", "sub_label": "{sub_label}", "obj_label": "{obj_label}", "count": xD' + "}\n")
+                        "{" + f'"uuid": "{predicate_id}_{c}", "predicate_id": "{predicate_id}", "sub_uri": "{sub_uri}", "sub_label": "{sub_label}", "obj_uri": "{objs[0]}", "obj_label": "{obj_label}"' + "}\n")
 
                     F_f.close()
 
-                    try:
-                        aliases = item["aliases"][lang]
-                    except:
-                        continue
+                    write_aliases(item)
 
-                    alias_f = open(f"own_alias/{lang}.txt", "a")
-                    alias_f.write(sub_uri + " ")
-                    for alias in aliases:
-                        alias_f.write(alias["value"])
-                        alias_f.write("    ")
-
-                    alias_f.write("\n")
-                    alias_f.close()
+                    c+=1
