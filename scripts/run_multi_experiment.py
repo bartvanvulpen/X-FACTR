1eeaa7ccd0e51bd97a396134b810f3d4ad4540ce
ScREy5 <pgeri595@gmail.com>
2022-10-10 19:36:26 +0200

    Final version with parameter grid search v2

--- scripts/run_multi_experiment.py
+++ scripts/run_multi_experiment.py
@@ -31,19 +31,33 @@ if __name__ == '__main__':
                     for iter_method in iter_methods:
                         for pid in pids: #pid number
 
-                            experiment = subprocess.run(["python", "scripts/probe_multi_pred.py",
-                                                         "--model", model,
-                                                         "--pids", pid,
-                                                         "--log_dir", f"experiment_results_multi_{mlang}/{plang}/{model}/{init_method}/{iter_method}",
-                                                         "--pred_dir", f"experiment_results_multi_{mlang}/{plang}/{model}/{init_method}/{iter_method}",
-                                                         "--lang", mlang,
-                                                         "--num_mask", str(num_mask),
-                                                         "--portion", "all",
-                                                         "--init_method", init_method,
-                                                         "--iter_method", iter_method,
-                                                         "--max_iter", str(num_mask),
-                                                        "--custom_facts", "own_facts_"+plang+"/"+pid+".jsonl"],
-                                                        shell=use_shell)
+                            if iter_method != "confidence-multi" :
+                                experiment = subprocess.run(["python", "scripts/probe_multi_pred.py",
+                                                             "--model", model,
+                                                             "--pids", pid,
+                                                             "--log_dir", f"experiment_results_multi_{mlang}/{plang}/{model}/{init_method}/{iter_method}",
+                                                             "--pred_dir", f"experiment_results_multi_{mlang}/{plang}/{model}/{init_method}/{iter_method}",
+                                                             "--lang", mlang,
+                                                             "--num_mask", str(num_mask),
+                                                             "--portion", "all",
+                                                             "--init_method", init_method,
+                                                             "--iter_method", iter_method,
+                                                             "--max_iter", str(num_mask),
+                                                            "--custom_facts", "own_facts_"+plang+"/"+pid+".jsonl"],
+                                                            shell=use_shell)
+                            else :
+                                experiment = subprocess.run(["python", "scripts/probe_multi_pred.py",
+                                                             "--model", model,
+                                                             "--pids", pid,
+                                                             "--log_dir", f"experiment_results_multi_{mlang}/{plang}/{model}/{init_method}/{iter_method}",
+                                                             "--pred_dir", f"experiment_results_multi_{mlang}/{plang}/{model}/{init_method}/{iter_method}",
+                                                             "--lang", mlang,
+                                                             "--num_mask", str(num_mask),
+                                                             "--portion", "all",
+                                                             "--init_method", init_method,
+                                                             "--iter_method", iter_method,
+                                                             "--custom_facts", "own_facts_" + plang + "/" + pid + ".jsonl"],
+                                                            shell=use_shell)
 
                             if experiment.returncode != 0 : raise RuntimeError("Experiment failed - Subprocess exit code 1")
 
